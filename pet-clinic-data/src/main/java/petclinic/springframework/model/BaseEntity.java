0b007652909a18711f5e3988f7979d82f9108a2f
Kochikame007 <43194027+Kochikame007@users.noreply.github.com>
2020-04-24 18:46:58 +0530

    Implementing base entity

---
+++ pet-clinic-data/src/main/java/petclinic/springframework/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package petclinic.springframework.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+	
+	private long Id;
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+	
+
+}
--- pet-clinic-data/src/main/java/petclinic/springframework/model/PetType.java
+++ pet-clinic-data/src/main/java/petclinic/springframework/model/PetType.java
@@ -1,5 +1,5 @@
 package petclinic.springframework.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
 }
