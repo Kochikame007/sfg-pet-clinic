2038b07a50d2cb46afeecf6141664df53d33ca88
Kochikame007 <43194027+Kochikame007@users.noreply.github.com>
2020-05-01 13:28:04 +0530

    create pettypemapservice

---
+++ pet-clinic-data/src/main/java/petclinic/springframework/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package petclinic.springframework.service;
+
+import petclinic.springframework.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/petclinic/springframework/service/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package petclinic.springframework.service.map;
+
+import java.util.Set;
+
+import petclinic.springframework.model.PetType;
+import petclinic.springframework.service.PetTypeService;
+
+public abstract class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService{
+	
+	@Override
+	public Set<PetType> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public PetType save(PetType o) {
+		// TODO Auto-generated method stub
+		return super.save( o);
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public void delete(PetType o) {
+		super.delete(o);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+
+}
