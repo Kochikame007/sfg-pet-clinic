1cedbabdbea337d0384c260ca77080857c217826
Kochikame007 <43194027+Kochikame007@users.noreply.github.com>
2020-09-15 14:26:46 +0530

    updated vet to JPA Entity . Close #37

--- pet-clinic-data/src/main/java/petclinic/springframework/model/Vet.java
+++ pet-clinic-data/src/main/java/petclinic/springframework/model/Vet.java
@@ -3,8 +3,20 @@ package petclinic.springframework.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="vets")
 public class Vet extends Person {
 
+	@ManyToMany(fetch =FetchType.EAGER)
+	@JoinTable(name="vets_specialties" ,joinColumns = @JoinColumn(name="vet_id") , inverseJoinColumns = @JoinColumn(name="speciality_id"))
 	private Set<Specialty> specialty = new HashSet<>();
 
 	public Set<Specialty> getSpecialty() {
