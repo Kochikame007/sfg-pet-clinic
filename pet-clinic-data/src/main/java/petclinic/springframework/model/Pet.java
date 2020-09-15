39fe9106cd213e144cfdb21cb634809aee08ccb0
Kochikame007 <43194027+Kochikame007@users.noreply.github.com>
2020-09-15 14:09:48 +0530

    map owner entity. Close #36

--- pet-clinic-data/src/main/java/petclinic/springframework/model/Pet.java
+++ pet-clinic-data/src/main/java/petclinic/springframework/model/Pet.java
@@ -2,12 +2,30 @@ package petclinic.springframework.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
+
+	@ManyToOne
+	@JoinColumn(name = "type_id")
 	private PetType pettype;
-	private LocalDate date;
+
+	@Column(name = "birth_date")
+	private LocalDate birthDate;
+
+	@Column(name = "name")
 	private String name;
+
 	public Owner getOwner() {
 		return owner;
 	}
@@ -25,11 +43,11 @@ public class Pet extends BaseEntity {
 	}
 
 	public LocalDate getDate() {
-		return date;
+		return birthDate;
 	}
 
-	public void setDate(LocalDate date) {
-		this.date = date;
+	public void setDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
 	}
 
 	public String getName() {
@@ -39,6 +57,5 @@ public class Pet extends BaseEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 
 }
