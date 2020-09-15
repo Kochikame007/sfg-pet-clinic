af15f94ef4518721933b4b5b0830ad9ce4b037f0
Kochikame007 <43194027+Kochikame007@users.noreply.github.com>
2020-09-15 13:54:51 +0530

    create base Entity . Close #35

--- pet-clinic-data/src/main/java/petclinic/springframework/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/petclinic/springframework/model/BaseEntity.java
@@ -2,8 +2,23 @@ package petclinic.springframework.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * @author Shivam
+ */
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long Id;
 
 	public Long getId() {
